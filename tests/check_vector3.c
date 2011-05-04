e574c116f8756132e87b28e562fb0b4d2672fd1f
Francesco Romani <fromani@gmail.com>
2011-05-04 23:27:34 +0200

    R1: a few more vector3 tests

--- tests/check_vector3.c
+++ tests/check_vector3.c
@@ -29,6 +29,62 @@ START_TEST(test_vector3Set)
 }
 END_TEST
 
+START_TEST(test_vector3SetFromVector)
+{
+    EON_Float X = 1.0, Y = -2.0, Z = 3.0;
+    EON_Vector3 V, W;
+
+    eon_Vector3Set(&V, X, Y, Z);
+    eon_Vector3Set(&W, V.X, V.Y, V.Z);
+
+    fail_unless(eon_floatAreEquals(V.X, W.X), "X component mis-set");
+    fail_unless(eon_floatAreEquals(V.Y, W.Y), "Y component mis-set");
+    fail_unless(eon_floatAreEquals(V.Z, W.Z), "Z component mis-set");
+}
+END_TEST
+
+START_TEST(test_vector3AreEqualsComp)
+{
+    EON_Float X = 1.0, Y = -2.0, Z = 3.0;
+    EON_Vector3 V, W;
+
+    eon_Vector3Set(&V, X, Y, Z);
+    eon_Vector3Set(&W, X, Y, Z);
+
+    fail_unless(eon_floatAreEquals(V.X, W.X), "X component mis-set");
+    fail_unless(eon_floatAreEquals(V.Y, W.Y), "Y component mis-set");
+    fail_unless(eon_floatAreEquals(V.Z, W.Z), "Z component mis-set");
+}
+END_TEST
+
+START_TEST(test_vector3AreEquals)
+{
+    EON_Float X = 1.0, Y = -2.0, Z = 3.0;
+    EON_Vector3 V, W;
+
+    eon_Vector3Set(&V, X, Y, Z);
+    eon_Vector3Set(&W, X, Y, Z);
+
+    fail_unless(eon_Vector3IsEqual(&V, &W), "vectors are different");
+}
+END_TEST
+
+
+START_TEST(test_vector3AddVSRAdd)
+{
+    EON_Float X = 1.0, Y = -2.0, Z = 3.0;
+    EON_Vector3 V, W, Q;
+
+    eon_Vector3Set(&V, X, Y, Z);
+    eon_Vector3Set(&W, X, Y, Z);
+
+    eon_Vector3Add(&V, &W, &Q);
+    eon_Vector3RAdd(&V, &W);
+
+    fail_unless(eon_Vector3IsEqual(&V, &Q), "vectors are different");
+}
+END_TEST
+
 
 /*************************************************************************/
 
@@ -36,6 +92,10 @@ TCase *eon3d_testCaseVector3(void)
 {
     TCase *tcVector3 = tcase_create("eon3d.core.vector3");
     tcase_add_test(tcVector3, test_vector3Set);
+    tcase_add_test(tcVector3, test_vector3SetFromVector);
+    tcase_add_test(tcVector3, test_vector3AreEquals);
+    tcase_add_test(tcVector3, test_vector3AreEqualsComp);
+    tcase_add_test(tcVector3, test_vector3AddVSRAdd);
 
     return tcVector3;
 }
