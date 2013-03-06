09d4fd15601abd61d6bcb99268e9359a3e225462
Francesco Romani <fromani@gmail.com>
2013-03-06 08:39:33 +0100

    reintroduce the plush texture reader
    but move it into the reader module. WIP.

--- modules/eon3dx_reader.c
+++ modules/eon3dx_reader.c
@@ -140,7 +140,7 @@ static EON_Obj *eonx_PLYRead(p_ply ply, const char *filename, EON_Mat *mat)
 }
 
 
-EON_Obj *EON_ReadPLYObj(const char *filename, EON_Mat *mat)
+EON_Obj *EONx_ReadPLYObj(const char *filename, EON_Mat *mat)
 {
     EON_Obj *obj = NULL;
     p_ply ply = ply_open(filename, NULL, 0, NULL);
@@ -153,12 +153,176 @@ EON_Obj *EON_ReadPLYObj(const char *filename, EON_Mat *mat)
 
 #else /* ! HAVE_RPLY */
 
-EON_Obj *EON_ReadPLYObj(const char *filename, EON_Mat *mat)
+EON_Obj *EONx_ReadPLYObj(const char *filename, EON_Mat *mat)
 {
     return NULL;
 }
 
 #endif /* HAVE_RPLY */
+
+/******************************************************************************
+taken from
+Plush Version 1.2
+read_pcx.c
+PCX Texture Reader
+Copyright (c) 1996-2000, Justin Frankel
+******************************************************************************/
+
+static EON_uInt eon_HiBit(EON_uInt16 x)
+{
+  EON_uInt i = 16, mask = 1<<15;
+  while (mask) {
+    if (x & mask) return i;
+    mask >>= 1; i--;
+  }
+  return 0;
+}
+
+static EON_uInt eon_OptimizeImage(EON_uChar *pal,
+                                  EON_uChar *data, EON_uInt32 len)
+{
+  EON_uChar colors[256], *dd = data;
+  EON_uChar remap[256];
+  EON_sInt32 lastused, firstunused;
+  EON_uInt32 x;
+  memset(colors,0,256);
+  for (x = 0; x < len; x ++) colors[(EON_uInt) *dd++] = 1;
+  lastused = -1;
+  for (x = 0; x < 256; x ++) remap[x] = (EON_uChar)x;
+  lastused = 255;
+  firstunused = 0;
+  for (;;) {
+    while (firstunused < 256 && colors[firstunused]) firstunused++;
+    if (firstunused > 255) break;
+    while (lastused >= 0 && !colors[lastused]) lastused--;
+    if (lastused < 0) break;
+	if (lastused <= firstunused) break;
+    pal[firstunused*3] = pal[lastused*3];
+    pal[firstunused*3+1] = pal[lastused*3+1];
+    pal[firstunused*3+2] = pal[lastused*3+2];
+    colors[lastused] = 0;
+    colors[firstunused] = 1;
+	  remap[lastused] = (EON_uChar) firstunused;
+  }
+  x = len;
+  while (x--) {
+      EON_uChar value = remap[(EON_uInt) *data];
+      *data++ = value;
+  }
+  return (lastused+1);
+}
+
+static EON_sInt eon_ReadPCX(char *filename, EON_uInt16 *width, EON_uInt16 *height,
+                            EON_uChar **pal, EON_uChar **data)
+{
+  EON_uInt16 sx, sy, ex, ey;
+  FILE *fp = fopen(filename,"rb");
+  EON_uChar *data2;
+  if (!fp) return -1;
+  fgetc(fp);
+  if (fgetc(fp) != 5) { fclose(fp); return -2; }
+  if (fgetc(fp) != 1) { fclose(fp); return -2; }
+  if (fgetc(fp) != 8) { fclose(fp); return -3; }
+  sx = fgetc(fp); sx |= fgetc(fp)<<8;
+  sy = fgetc(fp); sy |= fgetc(fp)<<8;
+  ex = fgetc(fp); ex |= fgetc(fp)<<8;
+  ey = fgetc(fp); ey |= fgetc(fp)<<8;
+  *width = ex - sx + 1;
+  *height = ey - sy + 1;
+  fseek(fp,128,SEEK_SET);
+  if (feof(fp)) { fclose(fp); return -4; }
+  *data = (EON_uChar *) malloc((*width) * (*height));
+  if (!*data) { fclose(fp); return -128; }
+  sx = *height;
+  data2 = *data;
+  do {
+    int xpos = 0;
+    do {
+      char c = fgetc(fp);
+      if ((c & 192) == 192) {
+        char oc = fgetc(fp);
+        c &= ~192;
+        do {
+          *(data2++) = oc;
+          xpos++;
+        } while (--c && xpos < *width);
+      } else {
+        *(data2++) = c;
+        xpos++;
+      }
+    } while (xpos < *width);
+  } while (--sx);
+  if (feof(fp)) { fclose(fp); free(*data); return -5; }
+  fseek(fp,-769,SEEK_END);
+  if (fgetc(fp) != 12) { fclose(fp); free(*data); return -6; }
+  *pal = (EON_uChar *) malloc(768);
+  if (!*pal) { fclose(fp); free(*data); return -7; }
+  fread(*pal,3,256,fp);
+  fclose(fp);
+  return 0;
+}
+
+static void eon_RescaleImage(EON_uChar *in, EON_uChar *out, EON_uInt inx,
+                             EON_uInt iny, EON_uInt outx, EON_uInt outy)
+{
+  EON_uInt x;
+  EON_uInt32 X, dX, dY, Y;
+  dX = (inx<<16) / outx;
+  dY = (iny<<16) / outy;
+  Y = 0;
+  do {
+    EON_uChar *ptr = in + inx*(Y>>16);
+    X = 0;
+    Y += dY;
+    x = outx;
+    do {
+      *out++ = ptr[X>>16];
+      X += dX;
+    } while (--x);
+  } while (--outy);
+}
+
+
+EON_Texture *EON_ReadPCXTex(char *fn, EON_Bool rescale, EON_Bool optimize)
+{
+  EON_uChar *data, *pal;
+  EON_uInt16 x, y;
+  EON_Texture *t;
+  if (eon_ReadPCX(fn,&x,&y,&pal,&data) < 0) return 0;
+  t = (EON_Texture *) malloc(sizeof(EON_Texture));
+  if (!t) return 0;
+  t->Width = eon_HiBit(x);
+  t->Height = eon_HiBit(y);
+  if (rescale && (1 << t->Width != x || 1 << t->Height != y)) {
+    EON_uChar nx, ny, *newdata;
+    nx = t->Width;
+    if ((1 << t->Width) != x) nx++;
+    ny = t->Height;
+    if ((1 << t->Height) != y) ny++;
+    newdata = (EON_uChar *) malloc((1<<nx)*(1<<ny));
+    if (!newdata) {
+      free(t);
+      free(data);
+      free(pal);
+      return 0;
+    }
+    eon_RescaleImage(data,newdata,x,y,1<<nx,1<<ny);
+    free(data);
+    data = newdata;
+    t->Width = nx;
+    t->Height = ny;
+    x = 1<<nx; y = 1<<ny;
+  }
+  t->iWidth = x;
+  t->iHeight = y;
+  t->uScale = (EON_Float) (1<<t->Width);
+  t->vScale = (EON_Float) (1<<t->Height);
+  if (optimize) t->NumColors = eon_OptimizeImage(pal, data,x*y);
+  else t->NumColors = 256;
+  t->Data = data;
+  t->PaletteData = pal;
+  return t;
+}
 
 
 /* vim: set ts=4 sw=4 et */
--- modules/eon3dx_reader.h
+++ modules/eon3dx_reader.h
@@ -20,6 +20,27 @@
  * 3. This notice may not be removed or altered from any source           *
  *    distribution.                                                       *
  *                                                                        *
+ * Includes code from plush, licensed under the following terms           *
+ * Copyright (C) 1996-1998, Justin Frankel                                *
+ * Copyright (C) 1998-2000, Nullsoft, Inc.                                *
+ *                                                                        *
+ * This software is provided 'as-is', without any express or implied      *
+ * warranty.  In no event will the authors be held liable for any damages *
+ * arising from the use of this software.                                 *
+ *                                                                        *
+ * Permission is granted to anyone to use this software for any purpose,  *
+ * including commercial applications, and to alter it and redistribute it *
+ * freely, subject to the following restrictions:                         *
+ *                                                                        *
+ * 1. The origin of this software must not be misrepresented; you must    *
+ *    not claim that you wrote the original software. If you use this     *
+ *    software in a product, an acknowledgment in the product             *
+ *    documentation would be appreciated but is not required.             *
+ * 2. Altered source versions must be plainly marked as such, and must    *
+ *    not be misrepresented as being the original software.               *
+ * 3. This notice may not be removed or altered from any source           *
+ *    distribution.                                                       *
+ *                                                                        *
  **************************************************************************/
 
 #ifndef EON3DX_READER_H
@@ -28,7 +49,10 @@
 #include <eon3d.h>
 
 
-EON_Obj *EON_ReadPLYObj(const char *filename, EON_Mat *mat);
+EON_Obj *EONx_ReadPLYObj(const char *filename, EON_Mat *mat);
+
+EON_Texture *EONx_ReadPCXTex(char *fn,
+                             EON_Bool rescale, EON_Bool optimize);
 
 #endif /* EON3DX_READER_H */
 
