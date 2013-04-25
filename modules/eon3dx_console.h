c586c4b02b25e80be090298305683bf38c5340a4
Francesco Romani <fromani@gmail.com>
2013-04-25 15:17:34 +0200

    more docs

--- modules/eon3dx_console.h
+++ modules/eon3dx_console.h
@@ -36,7 +36,7 @@ typedef struct eonx_console_ EONx_Console;
 typedef int (*EONx_KeyHandler)(int key, EONx_Console *con, void *userdata);
 
 /*
-  EON_ConsoleStartup() initializes the console backend.
+  EONx_ConsoleStartup() initializes the console backend.
   Parameters:
     title: title of the graphical window when in foreground
     icon: title of the graphical window when iconized (WARNING!)
@@ -51,7 +51,7 @@ typedef int (*EONx_KeyHandler)(int key, EONx_Console *con, void *userdata);
 int EONx_ConsoleStartup(const char *title, const char *icon);
 
 /*
-  EON_ConsoleShutdown() finalizes and releases the console backend.
+  EONx_ConsoleShutdown() finalizes and releases the console backend.
   Parameters:
     none
   Returns:
@@ -64,24 +64,111 @@ int EONx_ConsoleStartup(const char *title, const char *icon);
 */
 int EONx_ConsoleShutdown(void);
 
+/*
+  EONx_ConsoleNew() gets a new console instance. Depending on the
+  actual implementation, a console can have multiple independent
+  instance, multiple references to the same instance (aka the
+  singleton pattern) or exactly one instance.
+  Parameters:
+    sw: screen width (pixels) of the console
+    sh: screen height (pixels) of the console
+    fov: field of vision (degrees) of the console (you usually want
+         to use 90 here).
+  Returns:
+    NULL if a new console instance cannot be made, a valid pointer
+    otherwise
+  Notes:
+    a return value of NULL does NOT necessarely means an error
+    is occurred.
+*/
 EONx_Console *EONx_ConsoleNew(EON_uInt sw, EON_uInt sh, EON_Float fov);
 
+/*
+  EONx_ConsoleDelte() releases a console instance obtained through
+  EONx_ConsoleNew.
+  Parameters:
+    ctx: a pointer to the console to be freed.
+  Returns:
+    always NULL (and never fails.)
+*/
 void *EONx_ConsoleDelete(EONx_Console *ctx);
 
+/*
+  EONx_ConsoleGetCamera() obtains a pointer to a Camera instance
+  from a console instance. The returned pointer is a singleton.
+  There is no need to free() or destroy in any way the returned
+  pointer.
+  Parameters:
+    ctx: a pointer to a console instance.
+  Returns:
+    a pointer to a Camera bound to the given console instance.
+*/
 EON_Cam *EONx_ConsoleGetCamera(EONx_Console *ctx);
 
+/*
+  EONx_ConsoleBindEventKey() binds an handler to the event generated
+  by a key hit. The handler will block the event loop and
+  will run in the same thread running EONx_ConsoleNextEvent
+  Parameters:
+    ctx: console instance to bind the event/handler to.
+    key: value of the key to be handled. Can either be an ASCII
+         code or a SDL keymap value (XXX)
+    handler: event handling callback.
+    userdata: pointer to opaque data to be passed to the callback.
+  Returns:
+    0 on success, <0 on errors.
+*/
 int EONx_ConsoleBindEventKey(EONx_Console *ctx, int key,
                              EONx_KeyHandler handler, void *userdata);
 
+/*
+  EONx_ConsoleNextEvent() polls the console backend for the next
+  event to be processed, NOT blocking the caller if no events
+  are available. If an event is available, calls all handlers
+  registered for it, or silently discards it if no handlers are
+  available.
+  Parameters:
+    ctx: pointer to the console instance to use.
+  Returns:
+    if any handler has run, returns the value of the first handler
+    registered which returned !0 (e.g. the error of ther first
+    handler which run and failed).
+    if no handler has run, returns 0 un success or !0 on error.
+*/
 int EONx_ConsoleNextEvent(EONx_Console *ctx);
 
 int EONx_ConsoleSetPalette(EONx_Console *ctx,
                            const uint8_t *palette, int numcolors);
 
+/*
+  EONx_ConsoleGetCamera() obtains a pointer to a Camera instance
+  from a console instance. The returned pointer is a singleton.
+  There is no need to free() or destroy in any way the returned
+  pointer.
+  Parameters:
+    ctx: a pointer to a console instance.
+  Returns:
+    a pointer to a Camera bound to the given console instance.
+*/
 EON_Frame *EONx_ConsoleGetFrame(EONx_Console *ctx);
 
+/*
+  EONx_ConsoleClearFrame() clears the current console frame.
+  Parameters:
+    ctx: a pointer to a console instance.
+  Returns:
+    0 un success, <0 on error.
+*/
 int EONx_ConsoleClearFrame(EONx_Console *ctx);
 
+/*
+  EONx_ConsoleShowFrame() paint the console with the current
+  frame.
+  Parameters:
+    ctx: a pointer to a console instance.
+  Returns:
+    0 un success, <0 on error.
+*/
 int EONx_ConsoleShowFrame(EONx_Console *ctx);
 
 /* helpers */
